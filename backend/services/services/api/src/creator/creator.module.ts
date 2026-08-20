2b84152f9fdc2f22a65647d32eb007c2a47bcb5e
Paul Light <nwosupaul3@gmail.com>
2026-08-20 23:32:15 +0100

    feat(mentor): register secure proof upload

--- backend/services/services/api/src/creator/creator.module.ts
+++ backend/services/services/api/src/creator/creator.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
-import { CreatorService } from "./creator.service";
-import { CreatorController } from "./creator.controller";
 import { NotificationsModule } from "../notifications/notifications.module";
+import { CreatorController } from "./creator.controller";
+import { CreatorProofController } from "./creator-proof.controller";
+import { CreatorProofService } from "./creator-proof.service";
+import { CreatorService } from "./creator.service";
 
 @Module({
   imports: [NotificationsModule],
-  controllers: [CreatorController],
-  providers: [CreatorService],
-  exports: [CreatorService],
+  controllers: [CreatorController, CreatorProofController],
+  providers: [CreatorService, CreatorProofService],
+  exports: [CreatorService, CreatorProofService],
 })
 export class CreatorModule {}
