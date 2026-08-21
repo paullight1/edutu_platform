3864fa3f5ccc0e4af3e0b45366ca835fa218c7a0
Paul Light <nwosupaul3@gmail.com>
2026-08-21 08:20:36 +0100

    feat: register marketplace catalogue API

--- backend/services/services/api/src/creator/creator.module.ts
+++ backend/services/services/api/src/creator/creator.module.ts
@@ -4,11 +4,17 @@ import { CreatorController } from "./creator.controller";
 import { CreatorProofController } from "./creator-proof.controller";
 import { CreatorProofService } from "./creator-proof.service";
 import { CreatorService } from "./creator.service";
+import { MarketplaceCatalogController } from "./marketplace-catalog.controller";
+import { MarketplaceCatalogService } from "./marketplace-catalog.service";
 
 @Module({
   imports: [NotificationsModule],
-  controllers: [CreatorController, CreatorProofController],
-  providers: [CreatorService, CreatorProofService],
-  exports: [CreatorService, CreatorProofService],
+  controllers: [
+    CreatorController,
+    CreatorProofController,
+    MarketplaceCatalogController,
+  ],
+  providers: [CreatorService, CreatorProofService, MarketplaceCatalogService],
+  exports: [CreatorService, CreatorProofService, MarketplaceCatalogService],
 })
 export class CreatorModule {}
