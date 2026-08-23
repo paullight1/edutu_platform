8b67a44e774aa73d095227bf99f5546cbc7bbd20
Paul Light <nwosupaul3@gmail.com>
2026-08-24 00:51:50 +0100

    test(engine): scaffold run opportunity review

---
+++ admin/src/features/engine/components/RunOpportunityReview.tsx
@@ -0,0 +1,9 @@
+import type { ScrapedOpportunity } from "../model/types";
+
+export default function RunOpportunityReview({
+  opportunities: _opportunities,
+}: {
+  opportunities: ScrapedOpportunity[];
+}) {
+  return null;
+}
