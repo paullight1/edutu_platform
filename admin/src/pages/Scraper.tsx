132d09cb729694a2d5495ee3b023a095e9f09197
Paul Light <nwosupaul3@gmail.com>
2026-08-24 15:32:03 +0100

    refactor(engine): delete retired scraper compatibility file

--- admin/src/pages/Scraper.tsx
+++
@@ -1,3 +0,0 @@
-export { default } from "../features/engine/pages/EngineSourcesPage";
-export { default as EngineRunsPage } from "../features/engine/pages/EngineRunsPage";
-export { default as EngineStatusPage } from "../features/engine/pages/EngineStatusPage";
